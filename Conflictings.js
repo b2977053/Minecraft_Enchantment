bbcd9e8e48e50bff995a784e57b358abb556cfd4
steven <b2977053@venus.lit.edu.tw>
2019-09-29 02:59:32 +0800

    FirstCommit

---
+++ Conflictings.js
@@ -0,0 +1,27 @@
+var Conflictings = {
+	_1:[[
+		'smite',
+		'bane_of_arthropods',
+		'sharpness',
+	],[
+		'mending',
+		'infinity',
+	],[
+		'loyalty',
+		'riptide',
+	],[
+		'piercing',
+		'multishot',
+	],[
+		'projectile_protection',
+		'fire_protection',
+		'blast_protection',
+	],],
+	_2:[[
+		'silk_touch',
+		'fortune',
+	],[
+		'channeling',
+		'riptide',
+	]]
+};
---
+++ Items.js
@@ -0,0 +1,25 @@
+var Items = [
+{"name":"劍", "url":"https://minecraft.gamepedia.com/Sword", "DataValues":[{"Item":" Wooden Sword","Namespaced_ID":"wooden_sword"},{"Item":" Stone Sword","Namespaced_ID":"stone_sword"},{"Item":" Iron Sword","Namespaced_ID":"iron_sword"},{"Item":" Diamond Sword","Namespaced_ID":"diamond_sword"},{"Item":" Golden Sword","Namespaced_ID":"golden_sword"}]},
+{"name":"弓", "url":"https://minecraft.gamepedia.com/Bow", "DataValues":[{"Item":"Bow","Namespaced_ID":"bow"}]},
+{"name":"三叉戟", "url":"https://minecraft.gamepedia.com/Trident", "DataValues":[{"Item":"Trident","Namespaced_ID":"trident"}]},
+{"name":"弩", "url":"https://minecraft.gamepedia.com/Crossbow", "DataValues":[{"Item":"Crossbow","Namespaced_ID":"crossbow"}]},
+{"name":"頭盔", "url":"https://minecraft.gamepedia.com/Helmet", "DataValues":[{"Item":" Leather Cap","Namespaced_ID":"leather_helmet"},{"Item":" Chainmail Helmet","Namespaced_ID":"chainmail_helmet"},{"Item":" Iron Helmet","Namespaced_ID":"iron_helmet"},{"Item":" Diamond Helmet","Namespaced_ID":"diamond_helmet"},{"Item":" Golden Helmet","Namespaced_ID":"golden_helmet"}]},
+{"name":"海龜殼", "url":"https://minecraft.gamepedia.com/Turtle_Shell", "DataValues":[{"Item":"Turtle Shell","Namespaced_ID":"turtle_helmet"}]},
+{"name":"南瓜", "url":"https://minecraft.gamepedia.com/Pumpkin", "DataValues":[{"Item":"Carved Pumpkin","Namespaced_ID":"carved_pumpkin"}]},
+{"name":"胸甲", "url":"https://minecraft.gamepedia.com/Chestplate", "DataValues":[{"Item":" Leather Tunic","Namespaced_ID":"leather_chestplate"},{"Item":" Chainmail Chestplate","Namespaced_ID":"chainmail_chestplate"},{"Item":" Iron Chestplate","Namespaced_ID":"iron_chestplate"},{"Item":" Diamond Chestplate","Namespaced_ID":"diamond_chestplate"},{"Item":" Golden Chestplate","Namespaced_ID":"golden_chestplate"}]},
+{"name":"護腿", "url":"https://minecraft.gamepedia.com/Leggings", "DataValues":[{"Item":" Leather Pants","Namespaced_ID":"leather_leggings"},{"Item":" Chainmail Leggings","Namespaced_ID":"chainmail_leggings"},{"Item":" Iron Leggings","Namespaced_ID":"iron_leggings"},{"Item":" Diamond Leggings","Namespaced_ID":"diamond_leggings"},{"Item":" Golden Leggings","Namespaced_ID":"golden_leggings"}]},
+{"name":"靴子", "url":"https://minecraft.gamepedia.com/Boots", "DataValues":[{"Item":" Leather Boots","Namespaced_ID":"leather_boots"},{"Item":" Chainmail Boots","Namespaced_ID":"chainmail_boots"},{"Item":" Iron Boots","Namespaced_ID":"iron_boots"},{"Item":" Diamond Boots","Namespaced_ID":"diamond_boots"},{"Item":" Golden Boots","Namespaced_ID":"golden_boots"}]},
+{"name":"鎬", "url":"https://minecraft.gamepedia.com/Pickaxe", "DataValues":[{"Item":" Wooden Pickaxe","Namespaced_ID":"wooden_pickaxe"},{"Item":" Stone Pickaxe","Namespaced_ID":"stone_pickaxe"},{"Item":" Iron Pickaxe","Namespaced_ID":"iron_pickaxe"},{"Item":" Diamond Pickaxe","Namespaced_ID":"diamond_pickaxe"},{"Item":" Golden Pickaxe","Namespaced_ID":"golden_pickaxe"}]},
+{"name":"鍬", "url":"https://minecraft.gamepedia.com/Shovel", "DataValues":[{"Item":" Wooden Shovel","Namespaced_ID":"wooden_shovel"},{"Item":" Stone Shovel","Namespaced_ID":"stone_shovel"},{"Item":" Iron Shovel","Namespaced_ID":"iron_shovel"},{"Item":" Diamond Shovel","Namespaced_ID":"diamond_shovel"},{"Item":" Golden Shovel","Namespaced_ID":"golden_shovel"}]},
+{"name":"斧", "url":"https://minecraft.gamepedia.com/Axe", "DataValues":[{"Item":" Wooden Axe","Namespaced_ID":"wooden_axe"},{"Item":" Stone Axe","Namespaced_ID":"stone_axe"},{"Item":" Iron Axe","Namespaced_ID":"iron_axe"},{"Item":" Diamond Axe","Namespaced_ID":"diamond_axe"},{"Item":" Golden Axe","Namespaced_ID":"golden_axe"}]},
+{"name":"鋤", "url":"https://minecraft.gamepedia.com/Hoe", "DataValues":[{"Item":" Wooden Hoe","Namespaced_ID":"wooden_hoe"},{"Item":" Stone Hoe","Namespaced_ID":"stone_hoe"},{"Item":" Iron Hoe","Namespaced_ID":"iron_hoe"},{"Item":" Diamond Hoe","Namespaced_ID":"diamond_hoe"},{"Item":" Golden Hoe","Namespaced_ID":"golden_hoe"}]},
+{"name":"剪刀", "url":"https://minecraft.gamepedia.com/Shears", "DataValues":[{"Item":"Shears","Namespaced_ID":"shears"}]},
+{"name":"打火石", "url":"https://minecraft.gamepedia.com/Flint_and_Steel", "DataValues":[{"Item":"Flint and Steel","Namespaced_ID":"flint_and_steel"}]},
+{"name":"釣魚竿", "url":"https://minecraft.gamepedia.com/Fishing_Rod", "DataValues":[{"Item":"Fishing Rod","Namespaced_ID":"fishing_rod"}]},
+{"name":"胡蘿蔔釣竿", "url":"https://minecraft.gamepedia.com/Carrot_on_a_Stick", "DataValues":[{"Item":"Carrot on a Stick","Namespaced_ID":"carrot_on_a_stick"}]},
+{"name":"鞘翅", "url":"https://minecraft.gamepedia.com/Elytra", "DataValues":[{"Item":"Elytra","Namespaced_ID":"elytra"}]},
+{"name":"盾牌", "url":"https://minecraft.gamepedia.com/Shield", "DataValues":[{"Item":"Shield","Namespaced_ID":"shield"}]},
+]
+
+
+
